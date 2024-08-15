802cb98d4b7119517f3e7aab7e364ecce95105d3
Vaibhavthestudent <1ep22cs116@eastpoint.ac.in>
2024-08-15 14:18:14 +0530

    Added the contact me Section with Links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import link from '../assets/images/contact/linkedin.png'
+import insta from '../assets/images/contact/instagram.png'
+import github from '../assets/images/contact/github.png'
+import x from '../assets/images/contact/twitter.png'
 
+import '../index.css'
 function Contact() {
   return (
-    <section id="contact" className="container">
-      <h2>Contact Me</h2>
-      <p>Email: example@example.com</p>
-      <p>LinkedIn: <a href="https://www.linkedin.com">Profile</a></p>
+    <section id="contact" className="">
+      
+      <div className="contactsocial">
+      <a href="https://www.instagram.com/vaibhav.k111?igsh=dXNjbGJoazJjanY=">
+      <img src={insta} alt="insta" className='social'/></a>
+      <a href="https://x.com/Vaibhav_k111?t=WxTU764Tr-P4VBVwPdMGjg&s=08">
+      <img src={x} alt="twitter" className='social'/></a>
+      <a href="https://github.com/Vaibhavk121">
+      <img src={github} alt="github" className='social'/></a>
+      <a href="https://www.linkedin.com/in/vaibhav-kumar-b366872a6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+      <img src={link} alt="linkedin" className='social'/></a>
+      </div>
     </section>
   );
 }
