82855a06a636bf46f5d3ddb882ebd3046d0cda3a
Vaibhavk121 <158282913+Vaibhavk121@users.noreply.github.com>
2024-08-16 12:56:33 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-xl transition duration-300">
           <h3 className="text-xl font-semibold mb-2 text-gray-800">AI Chat bot</h3>
-          <p className="text-gray-600">An Ai tool for making your day to day life.</p>
+          <p className="text-gray-600">An Ai tool for making your day to day life easier.</p>
         </div>
         <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-xl transition duration-300">
           <h3 className="text-xl font-semibold mb-2 text-gray-800">Face detection tool</h3>
